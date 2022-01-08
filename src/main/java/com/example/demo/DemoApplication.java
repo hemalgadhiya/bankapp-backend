acdc9426cfe15c2a383e08c2a29537f57534c017
Gadhiya <gadhiy@3c22fb20bcce.ant.amazon.com>
2022-01-08 09:19:34 -0500

    docker expose

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,6 +10,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("App started....");
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+    	System.out.format("%s=%s%n", envName, env.get(envName));
+		}
+
 	}
 
 }
